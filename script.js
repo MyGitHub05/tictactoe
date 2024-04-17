b6ef5102f0aaf3f6d95e516c3fde2c354cb447d0
alecdev <alecvictorio2@gmail.com>
2024-04-17 22:56:25 +0800

    controller screen controler eventhandler

--- script.js
+++ script.js
@@ -1,20 +1,116 @@
 function GameBoard() {
-  const rows = 20;
-  const cols = 20;
+  const rows = 3;
+  const cols = 3;
   const board = [];
 
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < cols; j++) {
-      board[i].push("");
+      board[i].push(Cell());
     }
   }
 
   const getBoard = () => board;
 
   const playerMove = (column, player) => {
-    const availableCells = board.filter()
+    const availableCells = board
+      .filter((row) => row[column].getValue() === " ")
+      .map((row) => row[column]);
+
+    if (!availableCells) return;
+    availableCells[column] = player;
+
+    return availableCells;
   };
+
+  const printBoard = () => {
+    const boardWithCells = board.map((row) => {
+      row.map((cell) => {
+        cell.getValue();
+      });
+    });
+    console.log(boardWithCells);
+  };
+
+  return { getBoard, playerMove, printBoard };
 }
 
-console.log(GameBoard());
+function Cell() {
+  let value = "";
+  const dropMove = (player) => {
+    value = player;
+  };
+  const getValue = () => value;
+
+  return { dropMove, getValue };
+}
+
+function GameController(playerOne = "player 1", playerTwo = "player 2") {
+  const board = GameBoard();
+
+  const Player = [
+    {
+      name: playerOne,
+      token: "X",
+    },
+    {
+      name: playerTwo,
+      token: "O",
+    },
+  ];
+
+  const activePlayer = Player[0];
+  const switchActivePlayer = () =>
+    activePlayer === Player[0] ? Player[1] : Player[0];
+
+  const getActivePlayer = () => activePlayer;
+  const printNewRound = () => {
+    board.printBoard();
+  };
+  const playRound = (column) => {
+    board.dropMove(column, getActivePlayer().token);
+    switchActivePlayer();
+    printNewRound();
+  };
+
+  printNewRound();
+  return { getActivePlayer, playRound, getBoard: board.getBoard };
+}
+
+function ScreenController() {
+  const game = GameController();
+  const message = document.querySelector(".message");
+  const boardDiv = document.querySelector(".board");
+  const fullGame = document.querySelector(".game");
+
+  const updateScreen = () => {
+    fullGame.textContent = "";
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+    message.textContent = `${activePlayer.name}'s turn`;
+
+    board.forEach((row) => {
+      row.forEach((cell, index) => {
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        cellButton.dataset.column = index;
+        cellButton.textContent = cell.getValue();
+        boardDiv.appendChild(cellButton);
+      });
+    });
+  };
+
+  function ClickHandleBoard(e) {
+    const selectedColumn = e.target.dataset.column;
+
+    if (!selectedColumn) return;
+    game.playRound(selectedColumn);
+    updateScreen();
+  }
+
+  boardDiv.addEventListener("click", ClickHandleBoard);
+
+  updateScreen();
+}
+
+ScreenController();
